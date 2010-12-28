24ddce24fabe15faf3e2ecac8f47241f6f50364f
Jaro Šatkevič <jaro@akl.lt>
2010-12-28 04:52:06 +0200

    Creating default content

--- Products/Site/site.py
+++ Products/Site/site.py
@@ -61,8 +61,39 @@ class Site(Folder):
 
 InitializeClass(Site)
 
-
 # constructor pages. Only used when the product is added to a folder.
+
+def createDefaultIndex(context):
+    """Copying content form templates/index.pt to ZMI index_html Page Template"""
+
+    # Creting default index_html
+    f = open(ZOPE_HOME+'/Products/Site/templates/index.pt', 'r')
+    default_content = f.read()
+    f.close()
+    index_html = context.manage_addProduct['PageTemplates'].manage_addPageTemplate(
+        'index_html', text=default_content)
+
+    # Creating frontpage content (frontpage.pt)
+    f = open(ZOPE_HOME+'/Products/Site/templates/frontpage.pt', 'r')
+    fontpage_content = f.read()
+    f.close()
+    context.manage_addProduct['PageTemplates'].manage_addPageTemplate(
+        'frontpage.pt', text=fontpage_content)
+
+def createDefaultMenuItems(context):
+    """Creating elements needed for menu Items"""
+    menu_items = """
+return [
+    'about',
+    'contacts'
+]
+"""
+    context.manage_addProduct['PythonScripts'].manage_addPythonScript('menu_items')
+    context['menu_items'].ZPythonScript_edit('', menu_items)
+
+    context.manage_addProduct['OFSP'].manage_addFolder('about', 'About')
+    context.manage_addProduct['OFSP'].manage_addFolder('contacts', 'Contact us')
+
 
 def addAction(self, id='', title='', REQUEST=None):
             "Add a Site to Zope."
@@ -75,6 +106,10 @@ def addAction(self, id='', title='', REQUEST=None):
             site_root = getattr(self, id)
             site_root.manage_addProperty('header', '', 'string')
             site_root.manage_addProperty('description', '', 'text')
+
+            # Creating default content
+            createDefaultIndex(site_root)
+            createDefaultMenuItems(site_root)
 
             if REQUEST is not None:
                 return self.manage_main(self, REQUEST)
